eb2b5734c127d0c9ca5b2b34832bae25cef57b91
Graham McIntire <gmcintire@getaroom.com>
2019-11-26 10:37:02 -0600

    first commit

---
+++ lib/carprs.ex
@@ -0,0 +1,18 @@
+defmodule Carprs do
+  @moduledoc """
+  Documentation for Carprs.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Carprs.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/carprs_test.exs
@@ -0,0 +1,8 @@
+defmodule CarprsTest do
+  use ExUnit.Case
+  doctest Carprs
+
+  test "greets the world" do
+    assert Carprs.hello() == :world
+  end
+end
